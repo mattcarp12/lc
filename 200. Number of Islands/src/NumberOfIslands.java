b65e1bc5c64a09a255a6cc6b9ad83bd25ab968fc
mattcarp12 <mattcarp88@gmail.com>
2019-06-27 15:45:56 -0400

    completed

--- 200. Number of Islands/src/NumberOfIslands.java
+++ 200. Number of Islands/src/NumberOfIslands.java
@@ -1,18 +1,23 @@
 public class NumberOfIslands {
     char[][] grid;
     int count;
+    int rows;
+    int cols;
     DisjointSet components;
+
+    NumberOfIslands() {}
+
     class DisjointSet {
         private int[] nodes;
         private int[] size;
         public DisjointSet (char[][] grid) {
-            nodes = new int[grid.length*grid[0].length];
-            size = new int[grid.length*grid[0].length];
-            for (int i = 0; i < grid.length; i++) {
-                for (int j = 0; i < grid[i].length; j++) {
-                    if (grid[i][j] == '1') {nodes[i+j] = i+j; size[i+j] = 1;}
-                    else {nodes[i+j] = -1; size[i+j] = 0;}
-                }
+            int rows = grid.length;
+            int cols = grid[0].length;
+            nodes = new int[rows * cols];
+            size = new int[rows * cols];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++)
+                    { nodes[cols *i + j] = cols * i + j; size[cols *i + j] = 1; }
             }
         }
 
@@ -46,33 +51,49 @@ public class NumberOfIslands {
     }
 
     public int DepthFirstSearch(int i, int j) {
-        int count = 0;
+        this.count = 0;
         Dfs(i, j);
-        return count;
+        return this.count;
     }
 
     public void Dfs(int i, int j) {
-        count++;
-        components.union(i, j);
-        if (validIndex(i + 1, j) && !components.connected(i + 1, j) && grid[i + 1][j] == '1') Dfs(i + 1, j);
-        if (validIndex(i + 1, j) && !components.connected(i + 1, j) && grid[i + 1][j] == '1') Dfs(i, j + 1);
-        if (validIndex(i + 1, j) && !components.connected(i + 1, j) && grid[i + 1][j] == '1') Dfs(i, j - 1);
-        if (validIndex(i + 1, j) && !components.connected(i + 1, j) && grid[i + 1][j] == '1') Dfs(i - 1, j);
+        this.count++;
+        if (validIndex(i + 1, j) && !components.connected(cols*i + j, cols*(i + 1) + j) && grid[i + 1][j] == '1')
+            {this.components.union(cols*i + j, cols*(i + 1) + j); Dfs(i + 1, j);};
+        if (validIndex(i, j + 1) && !components.connected(cols*i + j, cols*i + j + 1) && grid[i][j + 1] == '1')
+            {this.components.union(cols*i + j, cols*i + j + 1); Dfs(i, j + 1);};
+        if (validIndex(i, j - 1) && !components.connected(cols*i + j, cols*i + j - 1) && grid[i][j - 1] == '1')
+            {this.components.union(cols*i + j, cols*i + j - 1); Dfs(i, j - 1);};
+        if (validIndex(i - 1, j) && !components.connected(cols*i + j, cols*(i - 1) + j) && grid[i - 1][j] == '1')
+            {this.components.union(cols*i + j, cols*(i - 1) + j); Dfs(i - 1, j);};
     }
 
-    public int numIslands(char[][] grid) {
-        this.grid = grid;
+    public int numIslands(char[][] grd) {
+        this.grid = grd;
         components = new DisjointSet(grid);
         int max_islands = 0;
         int islands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (components.isRoot(i+j)){ //only search if not already part of a component
+        int counter = 0;
+        rows = grid.length;
+        cols = grid[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (components.isRoot(cols*i+j) && grid[i][j] == '1'){ //only search if not already part of a component
                     islands = DepthFirstSearch(i, j);
-                    max_islands = Math.max(max_islands, islands);
+                    //max_islands = Math.max(max_islands, islands);
+                    counter++;
+
                 }
             }
         }
-        return max_islands;
+        //return max_islands;
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        //char[][] grid = {{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}};
+        char[][] grid = {{'1','1','0','0','0'},{'1','1','0','0','0'},{'0','0','1','0','0'},{'0','0','0','1','1'}};
+        NumberOfIslands test = new NumberOfIslands();
+        int count = test.numIslands(grid);
     }
 }
