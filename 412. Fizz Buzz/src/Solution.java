ef1c5a7e7fe7037332daad457dc80956e9c2cf08
mcarpenter <mattcarp88@gmail.com>
2019-07-10 14:01:50 -0400

    update

---
+++ 412. Fizz Buzz/src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        int i = 1;
+        List<String> list = new ArrayList();
+        while (i <= n) {
+            if (i % 15 == 0) list.add("FizzBuzz");
+            else if (i % 5 == 0) list.add("Buzz");
+            else if (i % 3 == 0) list.add("Fizz");
+            else list.add(Integer.toString(i));
+            i++;
+        }
+        return list;
+    }
+}
