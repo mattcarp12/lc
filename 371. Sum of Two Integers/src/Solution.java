0bd38ca00ae80b5378ebc546c90bc90a7df7a9b7
mcarpenter <mattcarp88@gmail.com>
2019-07-11 14:40:00 -0400

    update

---
+++ 371. Sum of Two Integers/src/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int getSum2(int a, int b) {
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+
+    public int getSum(int a, int b) {
+        return ((a&b)<<1) + (a^b);
+    }
+}
