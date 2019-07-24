0b42ad204dc6ad68c7b35a2e247f0f3ed2b28f14
mcarpenter <mattcarp88@gmail.com>
2019-07-24 07:25:54 -0400

    update

--- 647. Palindromic Substrings/src/Solution.java
+++ 647. Palindromic Substrings/src/Solution.java
@@ -8,7 +8,7 @@ class Solution {
         for (int i = 0; i < n; i++) isPalindrome[i][0] = isPalindrome[i][1] = true;
 
         /* Main double for loop */
-        for (int i = 2; i < n + 1; i++) { // outer loop for length of subarray
+        for (int i = 2; i < n + 1; i++) { // outer loop for length of substring
             for (int j = 0; j <= n - i; j++) { // inner loop for displacement from start of string
                 if (isPalindrome[j + 1][i - 2] && s.charAt(j) == s.charAt(i + j - 1)) {
                     isPalindrome[j][i] = true;
