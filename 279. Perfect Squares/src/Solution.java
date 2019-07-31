369bb50766b283ab007586fb60b550e2c1c04bca
mcarpenter <mattcarp88@gmail.com>
2019-07-31 15:24:10 -0400

    implement lc 279

---
+++ 279. Perfect Squares/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class Solution {
+
+    public int numSquares(int n) {
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        int level = 0;
+        queue.offer(n);
+
+        while (!queue.isEmpty()) {
+            level++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int val = queue.poll();
+                for (int j = 1; j <= Math.sqrt(val); j++) {
+                    if (val - j*j == 0) return level;
+                    queue.offer(val - j*j);
+                }
+            }
+        }
+        return level;
+    }
+}
