80074990f81e529a52567b89f423d47e790dd53c
mattcarp12 <mattcarp88@gmail.com>
2019-08-05 06:34:53 -0400

    implement twoSum

---
+++ 1/twoSum/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package lc.1.twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author mcarpenter
+ *
+ */
+
+public class Solution {
+	public static int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(nums[i]))
+				return new int[] { map.get(nums[i]), i };
+			map.put(target - nums[i], i);
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = {100, 50, 99, 20, 30, 48, 77};
+		int target = 127;
+		System.out.println(twoSum(nums, target));
+	}
+}
+
