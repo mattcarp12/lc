52a32e7f6f2479288e53213064494a49df02ed6a
mattcarp12 <mattcarp88@gmail.com>
2019-08-08 16:39:00 -0400

    update

---
+++ src/p207/NumberOfCourses/Solution.java
@@ -0,0 +1,39 @@
+package p207.NumberOfCourses;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    ArrayList<Integer>[] adjList;
+    boolean[] visited;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (prerequisites.length == 0) return true;
+        adjList = new ArrayList[numCourses];
+        visited = new boolean[numCourses];
+        boolean canFinish = true;
+        for (int i = 0; i < numCourses; i++) adjList[i] = new ArrayList<Integer>();
+        for (int[] edge : prerequisites) adjList[edge[1]].add(edge[0]);
+        for (int i = 0; i < numCourses; i++) {
+            if (canFinish == false) return false;
+            if (!visited[i]) {
+                canFinish = canFinish && Dfs(i, new HashSet<Integer>());
+            }
+        }
+        return true;
+    }
+    
+    private boolean Dfs(int node, Set recSet) {
+        visited[node] = true;
+        boolean canFinish = true;
+        recSet.add(node);
+        for (Integer edge : adjList[node]) {
+            if (recSet.contains(edge)) return false;
+            if (!visited[edge]) 
+                canFinish = canFinish && Dfs(edge, recSet);
+        }
+        recSet.remove(node);
+        return canFinish;
+    }
+    
+}
