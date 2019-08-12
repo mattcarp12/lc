ad8ffd723f789fd7d6fed2d2036425cb1f3f86dc
mattcarp12 <mattcarp88@gmail.com>
2019-08-12 07:12:24 -0400

    implement p206 Reverse Linked List

---
+++ src/p206/ReverseLinkedList/Solution.java
@@ -0,0 +1,21 @@
+package p206.ReverseLinkedList;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, next = null, curr = head;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
