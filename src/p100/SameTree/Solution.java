1ec41c66f23dded1d58bbe3601b9e37847ef346e
mattcarp12 <mattcarp88@gmail.com>
2019-08-20 07:34:21 -0400

    implement p100.SameTree

---
+++ src/p100/SameTree/Solution.java
@@ -0,0 +1,11 @@
+package p100.SameTree;
+
+import TreeNode.TreeNode;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
