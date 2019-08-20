04d1fb676d6b5284e52bcbcd9b9cd1bf48f94764
mattcarp12 <mattcarp88@gmail.com>
2019-08-20 07:35:38 -0400

    implement p104.MaximumDepthOfBinaryTree

---
+++ src/p104/MaximumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,10 @@
+package p104.MaximumDepthOfBinaryTree;
+
+import TreeNode.TreeNode;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
