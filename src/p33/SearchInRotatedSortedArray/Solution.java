25aebd7f0753cfc39b44f928e62cafdbaf319eee
mattcarp12 <mattcarp88@gmail.com>
2019-09-09 05:10:51 -0400

    implement p33.SearchInRotatedSortedArray (now working properly)

--- src/p33/SearchInRotatedSortedArray/Solution.java
+++ src/p33/SearchInRotatedSortedArray/Solution.java
@@ -3,23 +3,32 @@ package p33.SearchInRotatedSortedArray;
 public class Solution {
 
     public static int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
         return searchHelper(nums, 0, nums.length - 1, target);
     }
 
-    private static int searchHelper(int[] nums, int left, int right, int target) {
+    static int searchHelper(int[] nums, int left, int right, int target) {
         int median = (right + left) / 2;
         if (nums[median] == target) return median;
-        if (median == 0) return -1;
+        if (left >= right) return -1;
 
-        if ((nums[left] > nums[median] && target >= nums[left]) ||
-                (nums[left] < nums[median] && target < nums[left]))
-            return searchHelper(nums, left, median - 1, target);
+        if (nums[left] <= nums[median]) {
+            if (nums[left] <= target && target < nums[median])
+                return searchHelper(nums, left, median - 1, target);
+            else
+                return searchHelper(nums, median + 1, right, target);
+        } else {
+            if (nums[median] < target && target <= nums[right])
+                return searchHelper(nums, median + 1, right, target);
+            else
+                return searchHelper(nums, left, median - 1, target);
+        }
+    }
 
-        if ((nums[left] > nums[median] && target < nums[left]) ||
-                (nums[left] < nums[median] && target >= nums[left]))
-            return searchHelper(nums, median + 1, right, target);
-
-        return -1;
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5};
+        int target = 1;
+        int sol = search(nums, target);
     }
 
 }
