6cbf419660633a8d3000a340f1bbf318aee42536
mattcarp12 <mattcarp88@gmail.com>
2019-09-13 05:08:14 -0400

    implement p48.RotateImage (bug fixed)

--- src/p48/RotateImage/Solution.java
+++ src/p48/RotateImage/Solution.java
@@ -1,25 +1,28 @@
 package p48.RotateImage;
 
+import java.util.Arrays;
+
 class Solution {
     public static void rotate(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i < n / 2; i++) {
-            for (int j = 0; j < n - i * 2 - 1; j++) {
+            for (int j = i; j < n - i - 1; j++) {
 
-                // top left to top right
-                int temp1 = matrix[i][n - i - 1];
-                matrix[i][n - i - 1] = matrix[i][j];
+                // Store first value in temp variable
+                int temp = matrix[i][j];
 
-                // top right to bottom right
-                int temp2 = matrix[n - i - 1][n - i - 1];
-                matrix[n - i - 1][n - i - 1] = temp1;
+                // bottom right to top right
+                matrix[i][j] = matrix[n - j - 1][i];
 
                 // bottom right to bottom left
-                temp1 = matrix[n - i - 1][i];
-                matrix[n - i - 1][i] = temp2;
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
 
-                // bottom left to top left
-                matrix[i][j] = temp1;
+                //top right to bottom right
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+
+                // top left to top right
+                matrix[j][n - i - 1] = temp;
+
             }
         }
     }
@@ -27,6 +30,7 @@ class Solution {
     public static void main(String[] args) {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         rotate(matrix);
-        System.out.println(matrix.toString());
+        for (int[] row : matrix)
+            System.out.println(Arrays.toString(row));
     }
 }
