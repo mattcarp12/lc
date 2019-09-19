f70c6d205a051f9479599bac5065e16f5b8d66e5
mattcarp12 <mattcarp88@gmail.com>
2019-09-19 05:16:14 -0400

    implement p15.ThreeSum (working but too slow)

--- src/p15/ThreeSum/Solution.java
+++ src/p15/ThreeSum/Solution.java
@@ -4,28 +4,30 @@ import java.util.*;
 
 class Solution {
     public static List<List<Integer>> threeSum(int[] nums) {
-        HashMap<Integer, List<Integer>> indices = (HashMap) indices(nums);
+        Map<Integer, List<Integer>> indices = indices(nums);
         int[] numsSorted = nums.clone();
         Arrays.sort(numsSorted);
         int n = nums.length;
-        List<List<Integer>> solutionList = new ArrayList<>();
+        Set<List<Integer>> solutionSet = new HashSet<>();
+        int indexInSorted;
+        int originalIndex;
+        Integer[] solution;
+        List<Integer> originalIndices;
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
-                int indexInSorted = Arrays.binarySearch(numsSorted, -(nums[i] + nums[j]));
+                indexInSorted = Arrays.binarySearch(numsSorted, -(nums[i] + nums[j]));
                 if (indexInSorted >= 0) {
-                    List<Integer> originalIndices = indices.get(numsSorted[indexInSorted]);
-                    int originalIndex = getOriginalIndex(originalIndices, j);
+                    originalIndices = indices.get(numsSorted[indexInSorted]);
+                    originalIndex = getOriginalIndex(originalIndices, j);
                     if (originalIndex != -1) {
-                        List<Integer> solution = new ArrayList<>();
-                        solution.add(nums[i]);
-                        solution.add(nums[j]);
-                        solution.add(nums[originalIndex]);
-                        solutionList.add(solution);
+                        solution = new Integer[]{nums[i], nums[j], nums[originalIndex]};
+                        Arrays.sort(solution);
+                        solutionSet.add(Arrays.asList(solution));
                     }
                 }
             }
         }
-        return solutionList;
+        return new ArrayList(solutionSet);
     }
 
     private static Map<Integer, List<Integer>> indices(int[] nums) {
