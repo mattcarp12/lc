3bd8f8fa564901be0dd72957d762406fb65c579f
mattcarp12 <mattcarp88@gmail.com>
2019-09-26 05:46:58 -0400

    implement solution to p91.DecodeWays

--- src/p91/DecodeWays/Solution.java
+++ src/p91/DecodeWays/Solution.java
@@ -2,6 +2,17 @@ package p91.DecodeWays;
 
 class Solution {
     public int numDecodings(String s) {
-        return 0;
+        if (s.charAt(0) == '0') return 0;
+        int[] dp = new int[s.length()];
+        dp[0] = 1;
+        for (int i = 1; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            char prev = s.charAt(i - 1);
+            if (curr != '0') dp[i] = dp[i - 1];
+            if (prev != '0')
+                if (Integer.parseInt("" + prev + curr) <= 26)
+                    dp[i] = (i == 1) ? dp[i] + 1 : dp[i] + dp[i - 2];
+        }
+        return dp[dp.length - 1];
     }
 }
